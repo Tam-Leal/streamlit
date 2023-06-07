4dcd64d26f5721c99a27b60ada2c182728488f77
Tam <tamerleal22@gmail.com>
2023-06-06 22:00:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,10 @@ import streamlit as st
 
 def main():
      st.write("testando app")
+     if st.button('Say hello'):
+
+         st.write('Why hello there')
+     else:
+         st.write('Goodbye')
 
 main()
